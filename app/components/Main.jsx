9d1d43c2b59e1dc409adc2a4280fd5eef97489d2
boots525 <alexisalawson@gmail.com>
2017-03-21 11:23:03 -0400

    added clock component and testing

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,15 +1,14 @@
 var React = require('react');
 var Nav = require('Nav');
-var Clock = require('Clock');
 
 var Main = (props) => {
 		return(
 		<div>
-		<Nav/>
-		<div className="row">
-			<p>Main.jsx rendered</p>
-			{props.children}
-			<Clock/>
+			<Nav/>
+				<div className="row">
+					<div className="column small-centered medium-6 large-4">
+						{props.children}
+					</div>
 			</div>
 		</div>
 		);
